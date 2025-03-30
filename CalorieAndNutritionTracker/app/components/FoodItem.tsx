06b8243142e0e3ba59a8e08498dbb877e47b1456
Nikodem Brożyniak <nbrozyniak@gmail.com>
2025-03-30 19:50:42 +0200

    StyleSheet created

--- CalorieAndNutritionTracker/app/components/FoodItem.tsx
+++ CalorieAndNutritionTracker/app/components/FoodItem.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Text, View } from "react-native";
+import { Text, View, StyleSheet } from "react-native";
 import { EvilIcons } from "@expo/vector-icons";
 
 interface FoodItemProps {
@@ -11,24 +11,38 @@ interface FoodItemProps {
 
 const FoodItem: React.FC<FoodItemProps> = ({ name, calories, servingSize, brand }) => {
   return (
-    <View
-      style={{
-        backgroundColor: "gainsboro",
-        padding: 15,
-        borderRadius: 10,
-        flexDirection: "row",
-        alignItems: "center",
-      }}
-    >
+    <View style={styles.container}>
       <View style={{ flex: 1 }}>
-        <Text style={{ fontSize: 18, fontWeight: "bold", flexShrink: 1 }}>{name}</Text>
-        <Text style={{ color: "dimgray", flexShrink: 1 }}>
+        <Text style={styles.nameText}>{name}</Text>
+        <Text style={styles.info}>
           {calories} cal, {servingSize}, {brand}
         </Text>
       </View>
-      <EvilIcons name="plus" size={28} color="#378709" style={{ marginLeft: 10 }} />
+      <EvilIcons name="plus" size={28} color="#378709" style={styles.icon} />
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+    container: {
+    backgroundColor: "gainsboro",
+    padding: 15,
+    borderRadius: 10,
+    flexDirection: "row",
+    alignItems: "center",
+  },
+    nameText: {
+    fontSize: 18,
+    fontWeight: "bold",
+    flexShrink: 1,
+  },
+  info: {
+    color: "dimgray",
+    flexShrink: 1,
+  },
+  icon: {
+    marginLeft: 10
+},
+})
 
 export default FoodItem;
